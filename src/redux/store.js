d7c9ac454406493693e9dcd55c3f021ecabc6955
Michelle0805 <vasudaria85@gmail.com>
2024-04-22 14:01:50 +0800

    Refactored to async function

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+// store.js
+
+import { contactsSlice } from '../redux/contactsSlice';
+import { filterSlice } from '../redux/filterSlice';
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsSlice.reducer,
+    filter: filterSlice.reducer,
+}
+});
